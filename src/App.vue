7e5fd88816db916c0f89b83e3026d19fed29b2df
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-05-08 12:37:52 +0300

    nwe

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
         setTimeout(async()=>{
           try {
         const res = await fetch('https://yesno.wtf/api')
-        this.answer ="Your answer is -:"+ (await res.json()).answer +"🎉"
+        this.answer ="Your answer is -:"+ (await res.json()).answer=='yes'? "🎉" : "❌"
       } catch (error) {
         this.answer = 'Error! Could not reach the API. ' + error
       }
